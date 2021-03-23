5e8a331ad0768f2498e5db78d8f64e57cc8e8f07
suhee <suheeyoon@makinarocks.ai>
2021-03-23 13:15:16 +0900

    refactoring

--- batch labeling.py
+++ batch labeling.py
@@ -1,33 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import os, time
 import pandas as pd
 import pickle as pkl
+
+from skimage.transform import resize
+from scipy.spatial import distance
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score,f1_score
+from sklearn.cluster import KMeans, MiniBatchKMeans, DBSCAN
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
 
 import tensorflow as tf
 import keras
 from keras import layers, Input, models, Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.layers import Input, Dense, Conv2D, MaxPool2D, Dropout, Flatten, Activation, concatenate, GlobalAveragePooling2D
-from skimage.transform import resize
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix,accuracy_score,f1_score
 from keras.callbacks import EarlyStopping
 from keras.utils import to_categorical
 
 import matplotlib.pyplot as plt
-import time
-
-from sklearn.cluster import KMeans, MiniBatchKMeans, DBSCAN
-from sklearn.preprocessing import StandardScaler
-
-from scipy.spatial import distance
-
 
 # In[2]:
 
@@ -40,8 +33,6 @@ with open('WM.pkl','rb') as f:
     
 print(fea_all.shape, fea_all_tst.shape, len(X_rs), len(X_tst), y.shape, Y_tst.shape)
 
-
-# In[3]:
 
 
 # Number of each class
@@ -57,7 +48,6 @@ print("Number of Test Class", num_tst)
 
 # # Train / Validation / Test Split
 
-# In[4]:
 
 
 #n_tst = 10000
@@ -71,17 +61,11 @@ all_unique, all_counts = np.unique(np.where(y==1)[1], return_counts=True)
 all_k=dict(zip(all_unique, all_counts))
 
 
-# In[5]:
-
-
 # Standardize
 scaler = StandardScaler()
 
 fea_all=scaler.fit_transform(fea_all)
 fea_all_tst=scaler.fit_transform(fea_all_tst)
-
-
-# In[6]:
 
 
 def _permutation(set):
@@ -90,9 +74,6 @@ def _permutation(set):
         set[i] = set[i][permid]
 
     return set
-
-
-# In[ ]:
 
 
 # Random suffle array
@@ -185,19 +166,12 @@ print('::Dataset size','\n',
       'Test dataset size', X_tst.shape, Y_tst.shape)
 
 
-# In[ ]:
-
-
-#a=['Center','Donut','Edge-Loc','Edge-Ring','Loc','Random','Scratch','Near-full', 'none']
 plt.bar(unique, counts)
 plt.xlabel('Failure pattern')
 plt.ylabel('Number of queries')
 
 
 # # CNN Model
-
-# In[ ]:
-
 
 def create_model():
     dim = 64
@@ -218,11 +192,9 @@ def create_model():
                  loss='categorical_crossentropy',
                  metrics=['accuracy'])
     return model
+
 model = create_model()
 model.summary()
-
-
-# In[ ]:
 
 
 epoch=100
@@ -236,42 +208,13 @@ history = model.fit(X_trn, Y_trn,
      )
 
 
-# In[ ]:
-
-
 y_hat=np.argmax(model.predict(X_tst), axis=1)
 y_true = np.argmax(Y_tst,axis=1)
-
-
-# In[ ]:
 
 
 # performance metric
 print('Macro average F1 score:', f1_score(y_true, y_hat, average='macro'))
 print('Micro average F1 score:', f1_score(y_true, y_hat, average='micro'))
-
-
-# In[ ]:
-
-
-from sklearn.metrics import classification_report
 print(classification_report(y_true, y_hat))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
